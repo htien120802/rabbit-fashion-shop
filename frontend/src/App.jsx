c67fcdc8d59c2cc6b1070d59efcf2caf2805e5a5
htien120802 <dhtien120802@gmail.com>
2025-09-23 20:40:36 +0700

    Add page Login, Register, Profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Toaster } from 'sonner';
 import UserLayout from './components/Layout/UserLayout';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
         </Route>
         <Route></Route>
       </Routes>
